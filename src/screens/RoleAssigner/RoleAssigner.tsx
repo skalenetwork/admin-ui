39241c332f7f8ec6a3e532340152e1bc3d853092
Adil Shaikh <m.adilshaikh1@gmail.com>
2023-02-14 23:05:25 +0500

    impl basic role assigner

--- src/screens/RoleAssigner/RoleAssigner.tsx
+++ src/screens/RoleAssigner/RoleAssigner.tsx
@@ -1,14 +1,26 @@
 import Field from '@/elements/Field/Field';
 import { FormProvider, useForm } from 'react-hook-form';
-import { useAccount, useNetwork } from 'wagmi';
+import {
+  useAccount,
+  useContractWrite,
+  useNetwork,
+  usePrepareContractWrite,
+  useQuery,
+} from 'wagmi';
 
 import { CrownIcon } from '@/components/Icons/Icons';
 import { addresses } from '@/features/network';
 import { NETWORK } from '@/features/network/constants';
 import NotSupported from '@/screens/NotSupported';
 
+import { ContractDetailList } from '@/features/network/contract';
+import { useTypedContract } from '@/features/network/hooks';
+import { build, CONTRACT } from '@/features/network/manifest';
+
+import { useMemo } from 'react';
+
 type FormData = {
-  contractAddress: string;
+  contractAddress: ContractDetailList['address'];
   role: string;
   assigneeAddress: string;
 };
@@ -18,13 +30,49 @@ export default function RoleAssigner() {
     mode: 'all',
     reValidateMode: 'onChange',
     defaultValues: {
-      contractAddress: '',
+      contractAddress: CONTRACT['CONFIG_CONTROLLER'].address,
       role: '',
       assigneeAddress: '',
     } as FormData,
   });
   const { address } = useAccount();
   const { chain } = useNetwork();
+
+  const contractAddress = form.watch('contractAddress');
+  const role = form.watch('role');
+  const assigneeAddress = form.watch('assigneeAddress');
+
+  const selectedContractId = useMemo(() => {
+    return build.contractIdFromAddress(contractAddress);
+  }, [contractAddress]);
+
+  const { contract, abi } = useTypedContract({
+    id: selectedContractId,
+  });
+
+  const roles = abi
+    ? abi
+        .filter(
+          ({ type, name }) => type === 'function' && name.includes('_ROLE'),
+        )
+        .map((fragment) => fragment.name)
+    : [];
+
+  const { data: roleHash } = useQuery({
+    enabled: !!role,
+    queryKey: [selectedContractId, 'role', role],
+    queryFn: () => {
+      return contract?.[role]();
+    },
+  });
+
+  const { config } = usePrepareContractWrite({
+    abi,
+    address: contractAddress,
+    functionName: 'grantRole',
+    args: [roleHash, assigneeAddress],
+  });
+  const { writeAsync } = useContractWrite(config);
 
   return (
     <div
@@ -43,70 +91,76 @@ export default function RoleAssigner() {
       )}
       <div className="py-2">
         <p>Please fill in all inputs to assign role:</p>
-        <small className="text-gray-500">
-          @adil: implementation pending clear understanding of role matrix
-        </small>
       </div>
       <div className="grid grid-cols-2">
         <div>
           <FormProvider {...form}>
-            <Field<FormData>
-              name="contractAddress"
-              label="Contract"
-              control={() => (
-                <select>
-                  <option value="address">
-                    Options from <code>`@features/network`</code>
-                  </option>
-                </select>
-              )}
-              required="Contract is required"
-              placeholder="Choose a contract"
-            />
-            <Field<FormData>
-              name="role"
-              label="Role"
-              control={() => (
-                <select>
-                  <option value="address">
-                    Roles within scope of above contract
-                  </option>
-                </select>
-              )}
-              required="Contract is required"
-              placeholder="Choose a role"
-            />
-            <Field<FormData>
-              name="assigneeAddress"
-              label="Assignee"
-              control={() => <input type="text" />}
-              required="Assignee address is required"
-              placeholder="0x..."
+            <form
+              onSubmit={form.handleSubmit((data) => {
+                writeAsync?.();
+              })}
             >
-              <div className="my-2 flex flex-row gap-4">
-                <button
-                  className="btn slim text-sm"
-                  onClick={() => {
-                    form.setValue(
-                      'assigneeAddress',
-                      addresses.SCHAIN_MULTISIG_WALLET_ADDRESS as string,
-                    );
-                    form.trigger('assigneeAddress');
-                  }}
-                >
-                  Fill pre-deployed Multisig
-                </button>
-                <button
-                  className="btn slim text-sm"
-                  onClick={() => {
-                    form.setValue('assigneeAddress', address as string);
-                    form.trigger('assigneeAddress');
-                  }}
-                >
-                  Fill my address
-                </button>
-              </div>
-            </Field>
+              <Field<FormData>
+                name="contractAddress"
+                label="Contract"
+                control={() => (
+                  <select>
+                    {Object.values(CONTRACT).map((contract) => (
+                      <option value={contract.address}>{contract.name}</option>
+                    ))}
+                  </select>
+                )}
+                required="Contract is required"
+                placeholder="Choose a contract"
+              />
+              <Field<FormData>
+                name="role"
+                label="Role"
+                control={() => (
+                  <select>
+                    {roles.map((role) => (
+                      <option value={role}>{role}</option>
+                    ))}
+                  </select>
+                )}
+                required="Contract is required"
+                placeholder="Choose a role"
+              />
+              <Field<FormData>
+                name="assigneeAddress"
+                label="Assignee"
+                control={() => <input type="text" />}
+                required="Assignee address is required"
+                placeholder="0x..."
+              >
+                <div className="my-2 flex flex-row gap-4">
+                  <button
+                    className="btn btn-outline text-sm"
+                    onClick={() => {
+                      form.setValue(
+                        'assigneeAddress',
+                        addresses.SCHAIN_MULTISIG_WALLET_ADDRESS as string,
+                      );
+                      form.trigger('assigneeAddress');
+                    }}
+                  >
+                    Fill pre-deployed Multisig
+                  </button>
+                  <button
+                    className="btn btn-outline text-sm"
+                    onClick={() => {
+                      form.setValue('assigneeAddress', address as string);
+                      form.trigger('assigneeAddress');
+                    }}
+                  >
+                    Fill my address
+                  </button>
+                </div>
+              </Field>
+              <button type="submit" className="btn mt-8">
+                Assign Role
+              </button>
+            </form>
           </FormProvider>
         </div>
       </div>
